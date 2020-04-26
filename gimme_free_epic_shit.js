eecbea95673cc20fb82b9d2bbc8f6cdfb0f86e61
lupohan44 <lupohan44@users.noreply.github.com>
2020-04-26 13:47:26 +0800

    Update gimme_free_epic_shit.js
    Allow program to run infinity
    Support multi accounts

--- gimme_free_epic_shit.js
+++ gimme_free_epic_shit.js
@@ -1,73 +1,86 @@
 "use strict";
 const { "Launcher": EpicGames } = require("epicgames-client");
-const { email, password, rememberLastSession } = require(`${__dirname}/config.json`);
-const credentials = {
-    "email":    process.argv[2] || email,
-    "password": process.argv[3] || password
-};
-
-if (typeof process.argv[4] === "undefined") {
-    credentials.rememberLastSession = rememberLastSession;
-} else {
-    credentials.rememberLastSession = Boolean(Number(process.argv[4]));
-}
+const async = require("async");
+const sleep = (delay) => new Promise((res, rej) => setTimeout(res, delay * 1000));
 
 (async() => {
-    const client = new EpicGames(credentials);
+	do {
+		try {
+			var config = require(`${__dirname}/config.json`);
+			for (var accountId in config.accounts) {
+				var account = config.accounts[accountId];
+				const credentials = {
+					"email":    process.argv[2] || account.email,
+					"password": process.argv[3] || account.password
+				};
+				if (typeof process.argv[4] === "undefined") {
+					credentials.rememberLastSession = config.rememberLastSession;
+				} else {
+					credentials.rememberLastSession = Boolean(Number(process.argv[4]));
+				}
+				console.log(`Wait ${config.delay} seconds`);
+				await sleep(config.delay);
+				console.log(`Start to login email ${account.email}`);
+				var client = new EpicGames(credentials);
 
-    if (!await client.init()) {
-        throw new Error("Error while initialize process.");
-    }
+				if (!await client.init()) {
+					throw new Error("Error while initialize process.");
+				}
 
-    if (!await client.login().catch(() => false)) {
-        console.log(`Failed to login as ${client.config.email}, please attempt manually.`);
+				if (!await client.login().catch(() => false)) {
+					console.log(`Failed to login as ${client.config.email}, please attempt manually.`);
 
-        const ClientLoginAdapter = require("epicgames-client-login-adapter");
-        let auth = await ClientLoginAdapter.init(credentials);
-        let exchangeCode = await auth.getExchangeCode();
-        await auth.close();
+					var ClientLoginAdapter = require("epicgames-client-login-adapter");
+					let auth = await ClientLoginAdapter.init(credentials);
+					let exchangeCode = await auth.getExchangeCode();
+					await auth.close();
 
-        if (!await client.login(null, exchangeCode)) {
-            throw new Error("Error while logging in.");
-        }
-    }
+					if (!await client.login(null, exchangeCode)) {
+						throw new Error("Error while logging in.");
+					}
+				}
 
-    console.log(`Logged in as ${client.account.name} (${client.account.id})`);
-    let getAllOffers = async(namespace, pagesize = 100) => {
-        let i = 0;
-        let results = [];
-        while ((i * pagesize) - results.length === 0) {
-            let { elements } = await client.getOffersForNamespace(namespace, pagesize, pagesize * i++);
-            results = results.concat(elements);
-        }
-        return results;
-    };
+				console.log(`Logged in as ${client.account.name} (${client.account.id})`);
+				let getAllOffers = async(namespace, pagesize = 100) => {
+					let i = 0;
+					let results = [];
+					while ((i * pagesize) - results.length === 0) {
+						let { elements } = await client.getOffersForNamespace(namespace, pagesize, pagesize * i++);
+						results = results.concat(elements);
+					}
+					return results;
+				};
 
-    let all = await getAllOffers("epic");
-    let freegames = all
-        .filter(game => game.categories.find(cat => cat.path === "freegames")
-            && game.customAttributes["com.epicgames.app.offerNs"].value)
-        .map(game => game.customAttributes["com.epicgames.app.offerNs"].value);
+				let all = await getAllOffers("epic");
+				let freegames = all
+					.filter(game => game.categories.find(cat => cat.path === "freegames")
+						&& game.customAttributes["com.epicgames.app.offerNs"].value)
+					.map(game => game.customAttributes["com.epicgames.app.offerNs"].value);
 
-    for (let namespace of freegames) {
-        let offers = await getAllOffers(namespace);
-        let freeoffers = offers.filter(game => game.currentPrice === 0 && game.discountPercentage === 0);
+				for (let namespace of freegames) {
+					let offers = await getAllOffers(namespace);
+					let freeoffers = offers.filter(game => game.currentPrice === 0 && game.discountPercentage === 0);
 
-        for (let offer of freeoffers) {
-            let purchased = await client.purchase(offer, 1);
+					for (let offer of freeoffers) {
+						let purchased = await client.purchase(offer, 1);
 
-            if (purchased) {
-                console.log(`Successfully claimed ${offer.title} (${purchased})`);
-            } else {
-                console.log(`${offer.title} was already claimed for this account`);
-            }
-        }
-    }
+						if (purchased) {
+							console.log(`Successfully claimed ${offer.title} (${purchased})`);
+						} else {
+							console.log(`${offer.title} was already claimed for this account`);
+						}
+					}
+				}
 
-    await client.logout();
-    console.log("Logged out of Epic Games");
-    process.exit(0);
+				await client.logout();
+				console.log(`Logged out of Epic Games (account ${client.account.name})`);
+			}
+		}
+		catch(e) {
+			console.error(e);
+		}
+	}
+	while(config.loop);
 })().catch(err => {
-    console.error(err);
-    process.exit(1);
+	console.error(err);
 });
