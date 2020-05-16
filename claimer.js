78c7cfa7be54d80aa5e4b90a6bf76b0d0bf57a08
Maximous Black <maximousblk@gmail.com>
2020-05-16 16:05:56 +0530

    Updated README
    and separated CHANGELOG
    and accidentally changed the formatting

--- claimer.js
+++ claimer.js
@@ -1,5 +1,5 @@
 "use strict";
-const { "Launcher": EpicGames } = require("epicgames-client");
+const { Launcher: EpicGames } = require("epicgames-client");
 const CheckUpdate = require("check-update-github");
 const ClientLoginAdapter = require("epicgames-client-login-adapter");
 const Config = require(`${__dirname}/config.json`);
@@ -10,37 +10,42 @@ const TwoFactor = require("node-2fa");
 
 function isUpToDate() {
     return new Promise((res, rej) => {
-        CheckUpdate({
-            "name":           Package.name,
-            "currentVersion": Package.version,
-            "user":           "revadike",
-            "branch":         "master"
-        }, (err, latestVersion) => {
-            if (err) {
-                rej(err);
-            } else {
-                res(latestVersion === Package.version);
+        CheckUpdate(
+            {
+                name: Package.name,
+                currentVersion: Package.version,
+                user: "revadike",
+                branch: "master",
+            },
+            (err, latestVersion) => {
+                if (err) {
+                    rej(err);
+                } else {
+                    res(latestVersion === Package.version);
+                }
             }
-        });
+        );
     });
 }
 
-(async() => {
-    if (!await isUpToDate()) {
+(async () => {
+    if (!(await isUpToDate())) {
         Logger.warn(`There is a new version available: ${Package.url}`);
     }
 
     let { accounts, delay, loop } = Config;
-    let sleep = delay => new Promise(res => setTimeout(res, delay * 60000));
+    let sleep = (delay) => new Promise((res) => setTimeout(res, delay * 60000));
     do {
         if (process.argv.length > 2) {
             loop = false;
-            accounts = [{
-                "email":               process.argv[2],
-                "password":            process.argv[3],
-                "rememberLastSession": Boolean(Number(process.argv[4])),
-                "secret":              process.argv[5],
-            }];
+            accounts = [
+                {
+                    email: process.argv[2],
+                    password: process.argv[3],
+                    rememberLastSession: Boolean(Number(process.argv[4])),
+                    secret: process.argv[5],
+                },
+            ];
         }
 
         for (let account of accounts) {
@@ -52,43 +57,59 @@ function isUpToDate() {
 
             let client = new EpicGames(account);
 
-            if (!await client.init()) {
+            if (!(await client.init())) {
                 throw new Error("Error while initialize process.");
             }
 
-            if (!await client.login().catch(() => false)) {
-                Logger.warn(`Failed to login as ${client.config.email}, please attempt manually.`);
+            if (!(await client.login().catch(() => false))) {
+                Logger.warn(
+                    `Failed to login as ${client.config.email}, please attempt manually.`
+                );
 
                 let auth = await ClientLoginAdapter.init(account);
                 let exchangeCode = await auth.getExchangeCode();
                 await auth.close();
 
-                if (!await client.login(null, exchangeCode)) {
+                if (!(await client.login(null, exchangeCode))) {
                     throw new Error("Error while logging in.");
                 }
             }
 
-            Logger.info(`Logged in as ${client.account.name} (${client.account.id})`);
+            Logger.info(
+                `Logged in as ${client.account.name} (${client.account.id})`
+            );
 
             if (noSecret) {
-                await client.enableTwoFactor("authenticator", secret => {
+                await client.enableTwoFactor("authenticator", (secret) => {
                     account.secret = secret;
                 });
             }
 
-            let { data } = await client.http.sendGraphQL(null, PROMO_QUERY, { "category": "freegames", "locale": "en-US" });
+            let { data } = await client.http.sendGraphQL(null, PROMO_QUERY, {
+                category: "freegames",
+                locale: "en-US",
+            });
             let { elements } = JSON.parse(data).data.Catalog.searchStore;
-            let freePromos = elements.filter(offer => offer.promotions
-                && offer.promotions.promotionalOffers.length > 0
-                && offer.promotions.promotionalOffers[0].promotionalOffers.find(p => p.discountSetting.discountPercentage === 0));
+            let freePromos = elements.filter(
+                (offer) =>
+                    offer.promotions &&
+                    offer.promotions.promotionalOffers.length > 0 &&
+                    offer.promotions.promotionalOffers[0].promotionalOffers.find(
+                        (p) => p.discountSetting.discountPercentage === 0
+                    )
+            );
 
             for (let offer of freePromos) {
                 try {
                     let purchased = await client.purchase(offer, 1);
                     if (purchased) {
-                        Logger.info(`Successfully claimed ${offer.title} (${purchased})`);
+                        Logger.info(
+                            `Successfully claimed ${offer.title} (${purchased})`
+                        );
                     } else {
-                        Logger.info(`${offer.title} was already claimed for this account`);
+                        Logger.info(
+                            `${offer.title} was already claimed for this account`
+                        );
                     }
                 } catch (err) {
                     Logger.warn(`Failed to claim ${offer.title} (${err})`);
@@ -110,7 +131,7 @@ function isUpToDate() {
             process.exit(0);
         }
     } while (loop);
-})().catch(err => {
+})().catch((err) => {
     Logger.error(err);
     process.exit(1);
 });
--- logger.js
+++ logger.js
@@ -4,36 +4,43 @@ const fs = require("fs");
 const path = require("path");
 
 module.exports = {
-    "format":     " {{timestamp}} | {{title}} | {{message}}",
-    "dateformat": "yyyy-mm-dd | HH:MM:ss.l",
-    "filters":    {
-        "log":   colors.white,
-        "trace": colors.magenta,
-        "debug": colors.blue,
-        "info":  colors.green,
-        "warn":  colors.magenta,
-        "error": [colors.red, colors.bold]
+    format: " {{timestamp}} | {{title}} | {{message}}",
+    dateformat: "yyyy-mm-dd | HH:MM:ss.l",
+    filters: {
+        log: colors.white,
+        trace: colors.magenta,
+        debug: colors.blue,
+        info: colors.green,
+        warn: colors.magenta,
+        error: [colors.red, colors.bold],
     },
-    "preprocess": data => {
+    preprocess: (data) => {
         data.title = data.title.toUpperCase();
-        while (data.title.length < 5) { data.title += " "; }
+        while (data.title.length < 5) {
+            data.title += " ";
+        }
         data.args = [...data.args];
         if (data.args[0] && data.args[0].logpath) {
             data.logpath = data.args[0].logpath;
             data.args.shift();
         }
     },
-    "transport": data => {
+    transport: (data) => {
         // eslint-disable-next-line no-console
         console.log(data.output);
 
         const streamoptions = {
-            "flags":    "a",
-            "encoding": "utf8"
+            flags: "a",
+            encoding: "utf8",
         };
-        fs.createWriteStream(path.join(__dirname, "claimer.log"), streamoptions).write(`\r\n${data.rawoutput}`);
+        fs.createWriteStream(
+            path.join(__dirname, "claimer.log"),
+            streamoptions
+        ).write(`\r\n${data.rawoutput}`);
         if (data.logpath) {
-            fs.createWriteStream(data.logpath, streamoptions).write(`\r\n${data.rawoutput}`);
+            fs.createWriteStream(data.logpath, streamoptions).write(
+                `\r\n${data.rawoutput}`
+            );
         }
-    }
+    },
 };
