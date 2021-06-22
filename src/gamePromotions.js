dc2d3058c790c2046c03f318c387431e83dfc2e0
Zae <zaevi@live.com>
2021-06-22 20:05:20 +0800

    fix freeGamesPromotions

--- src/gamePromotions.js
+++ src/gamePromotions.js
@@ -18,7 +18,7 @@ async function freeGamesPromotions(client, country = "US", allowCountries = "US"
         let page = offer;
 
         if (offer.pages) {
-            page = offer.pages.find((p) => p._urlPattern.includes(promo.productSlug));
+            page = offer.pages.find((p) => p.offer.id == promo.id);
         }
 
         if (!page) {
